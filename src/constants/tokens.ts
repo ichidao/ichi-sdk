6a74194bdfcdc6b7bbd8600932ff0f54f8f09ce1
Tatyana Stemkovski <tanya_stemkovski@hotmail.com>
2022-09-04 23:01:54 -0400

    Target Valte Strength for ICHI

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -65,7 +65,7 @@ export const TOKENS: TokenMapping = {
       symbol: 'ICHI',
       isOneToken: false,
       atCoingecko: true,
-      targetVaultStrength: .1
+      targetVaultStrength: .11
     },
     [ChainId.Polygon]: {
       tokenName: TokenName.ICHI_V2,
@@ -76,7 +76,7 @@ export const TOKENS: TokenMapping = {
       symbol: 'ICHI',
       atCoingecko: false,
       isOneToken: false,
-      targetVaultStrength: .1
+      targetVaultStrength: .11
     }
     // [ChainId.Mumbai]: {
     //   tokenName: TokenName.ICHI_V2,
